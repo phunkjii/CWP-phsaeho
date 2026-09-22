660d8c4dd43015f11074ab516a2f562ec4d0accc
phunkjii <phongsakon.saeho@gmail.com>
2026-09-23 05:49:11 +0700

    t

---
+++ cell01/ex00/name.py
@@ -0,0 +1,4 @@
+first_name = "Phongsakon"
+last_name = "Saeho"
+
+print(first_name, last_name)
---
+++ cell01/ex01/name.py
@@ -0,0 +1,6 @@
+first_name = "Phongsakon"
+last_name = "Saeho"
+
+whole_name = first_name + " " + last_name
+
+print(whole_name)
