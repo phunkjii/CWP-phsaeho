f21f30bca613d7b4b9357cbca121a6237963f9ae
phunkjii <phongsakon.saeho@gmail.com>
2026-09-23 22:51:22 +0700

    t

---
+++ cell02/ex02/password.py
@@ -0,0 +1,10 @@
+#!/usr/bin/env python3
+
+password = "Python is awesome"
+
+entered_password = input()
+
+if entered_password == password:
+    print("ACCESS GRANTED")
+else:
+    print("ACCESS DENIED")
